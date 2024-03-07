69a8a3e3f0efa9a532576a7e212559f175eb1baf
ArzygulIm <arzygul.imankulova.kk@gmail.com>
2024-03-07 12:00:00 +0600

    for green git

--- js/layouts/first/script.js
+++ js/layouts/first/script.js
@@ -1,14 +1,10 @@
 // https://www.figma.com/file/yazf9gzkw52mCJNNxAGqS0/gifts-%26-merch?node-id=0%3A1
 
 const handleToggleBurger = () => {
-
-document.getElementById('burger').classList.toggle = 'show-nav'
-
-}
+  document.getElementById("burger").classList.toggle = "show-nav";
+};
 
 const mainCarousel = () => {
-    const slides = document.querySelectorAll("popular-carousel__carousel__item")
-    
-}
-mainCarousel()
-
+  const slides = document.querySelectorAll("popular-carousel__carousel__item");
+};
+mainCarousel();
