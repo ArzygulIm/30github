6f655a747390533a90c968464d7c71815dc58ccd
ArzygulIm <arzygul.imankulova.kk@gmail.com>
2025-02-06 14:43:44 +0600

    third day

---
+++ codewars/python.py
@@ -0,0 +1,15 @@
+# 1
+# def paperwork(n, m):
+#     return 0 if n < 0 or m < 0 else n*m
+
+# 2
+# def enough(cap, on, wait):
+#     return max(0, wait - (cap - on))
+
+# enough = lambda c,o,w: max(0, w-(c-o))
+
+# 3
+# def hero(bullets, dragons):
+#     return bullets//2>=dragons
+
+# 4
