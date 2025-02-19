3bd6988bfaf5304dbaec7953f8735dcc9d88aa1e
Arzygul <100558515+ArzygulIm@users.noreply.github.com>
2025-02-19 20:42:15 +0600

    sixteenth day

--- js/layouts/first/script.js
+++ js/layouts/first/script.js
@@ -1,2 +1,8 @@
 // https://www.figma.com/file/yazf9gzkw52mCJNNxAGqS0/gifts-%26-merch?node-id=0%3A1
 
+const handleToggleBurger = () => {
+
+document.getElementById('burger').classList.toggle = 'show-nav'
+
+}
+
