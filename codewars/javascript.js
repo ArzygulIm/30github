f24ac3d6a1644236a01cca15d9f49ed38daff19c
Arzygul <100558515+ArzygulIm@users.noreply.github.com>
2025-03-01 12:11:12 +0600

    Update javascript.js

--- codewars/javascript.js
+++ codewars/javascript.js
@@ -64,3 +64,35 @@ function powersOfTwo(n) {
   return Array.from({length: n + 1}, (v, k) => 2 ** k);
 }
 
+//5
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+  let arr=s.split(' ')
+  let min=s.length
+  for( i of arr){
+    if( i.length<min){
+      min=i.length
+    }
+  }
+  return min
+}
+
+function findShort(s){
+    return Math.min(...s.split(" ").map (s => s.length));
+}
+
+function findShort(s){
+  return Math.min.apply(null, s.split(' ').map(w => w.length));
+}
+
+const findShort = (s) => s
+  .split(' ')
+  .sort((a, b) => b.length - a.length)
+  .pop()
+  .length;
+
+
