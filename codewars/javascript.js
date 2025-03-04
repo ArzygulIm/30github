8dfeef7693bb6960ae640f587e6f2350d1cee5b8
Arzygul <100558515+ArzygulIm@users.noreply.github.com>
2025-03-04 23:03:38 +0600

    twenty nineth day

--- codewars/javascript.js
+++ codewars/javascript.js
@@ -126,5 +126,12 @@ function expandedForm(num) {
     .join(' + ');
 }
 
+//8
+Complete the solution so that it reverses the string passed into it.
 
+'world'  =>  'dlrow'
+'word'   =>  'drow'
 
+function solution(str){
+  return str.split('').reverse().join('')
+}
